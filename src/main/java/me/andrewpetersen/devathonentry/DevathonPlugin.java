2d72f011f6c28f9affff95ff88bdf35105a5121b
Andrew <andy1ne0@gmail.com>
2016-11-06 09:24:32 +1000

    Initial commit, started to prepare the groundway for the rest of the project.

---
+++ src/main/java/me/andrewpetersen/devathonentry/DevathonPlugin.java
@@ -0,0 +1,54 @@
+package me.andrewpetersen.devathonentry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public class DevathonPlugin extends JavaPlugin {
+
+    private static DevathonPlugin instance = null;
+
+    /**
+     * Get the Devathon Instance.
+     *
+     * @return The plugin instance.
+     */
+    public static DevathonPlugin getInstance() {
+        return DevathonPlugin.instance;
+    }
+
+    /**
+     * Set the DevathonPlugin instance. Throws an IllegalStateException if the instance has already been set.
+     *
+     * @param instance The plugin instance.
+     */
+    public static void setInstance(DevathonPlugin instance) {
+
+        if (DevathonPlugin.getInstance() != null) {
+            throw new IllegalStateException("The instance has already been set! ");
+        }
+        DevathonPlugin.instance = instance;
+    }
+
+    /**
+     * The verbose method - any strings sent to this method will be logged using the verbose layout.
+     *
+     * @param msg The verbose message to be logged.
+     */
+    public void verbose(String msg) {
+        this.getServer().getLogger().log(Level.INFO, Strings.VERBOSE_PREFIX + msg);
+    }
+
+    @Override
+    public void onEnable() {
+        DevathonPlugin.setInstance(this);
+        this.getServer().getLogger().log(Level.INFO, "The Devathon project plugin has been initialized. ");
+        this.getServer().getLogger().log(Level.INFO, "For usage instructions (how to create the machine), please see the README.md file in the Git repo. ");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getServer().getLogger().log(Level.INFO, "The Devathon machine project has been disabled. ");
+    }
+}
+
---
+++ src/main/java/me/andrewpetersen/devathonentry/Strings.java
@@ -0,0 +1,17 @@
+package me.andrewpetersen.devathonentry;
+
+/*
+ * This project has been written by Andrew Petersen, and anyone who has contributed to the source code
+ * (or where otherwise declared). 
+ *
+ * This code is licensed under the GPLv3 License, a copy of which can be found in the root directory. 
+ */
+
+/**
+ * This is the class where any strings that should be used will be stored.
+ */
+public class Strings {
+
+    static final String VERBOSE_PREFIX = "[VERBOSE] ";
+
+}
