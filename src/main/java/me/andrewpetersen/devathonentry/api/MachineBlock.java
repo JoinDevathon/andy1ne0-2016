079b825be753f07f5f60584cd8eb3f3b0be1e34f
Andrew <andy1ne0@gmail.com>
2016-11-06 10:05:58 +1000

    Added some per-block fields to the API, constructor work.

--- src/main/java/me/andrewpetersen/devathonentry/api/MachineBlock.java
+++ src/main/java/me/andrewpetersen/devathonentry/api/MachineBlock.java
@@ -7,9 +7,10 @@ package me.andrewpetersen.devathonentry.api;
  * This code is licensed under the GPLv3 License, a copy of which can be found in the root directory. 
  */
 
-import jdk.nashorn.internal.ir.Block;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 
 /**
  * The wrapper class for any blocks that need to be generated when creating a machine.
@@ -28,15 +29,34 @@ public class MachineBlock {
 
     private boolean sparkleEffect = false;
 
-    private boolean soundEffects = false;
-
     /**
      * The default constructor for this class, used to set various variables.
      *
      * @param master The master MachineLevel instance.
+     * @param block The block that this class manipulates.
+     * @param m The material this block will be set to.
      */
-    public MachineBlock(MachineLevel master) {
+    public MachineBlock(MachineLevel master, Block block, Material m) {
         this.setMasterLevel(master);
+        this.setBlock(block);
+    }
+
+    /**
+     * The constructor used, should any generic effects be needed. Note that sound effects should be handled in the master machine level class.
+     *
+     * @param master        The master MachineLevel instance.
+     * @param block         The block that this class manipulates.
+     * @param m             The material this block will be set to.
+     * @param explodeEffect Whether an explosion effect should be used post-creation of this block.
+     * @param smokeEffect   Whether a smoke effect should be used post-creation of this block.
+     * @param sparkleEffect Whether a sparkle effect should be used post-creation of this block.
+     */
+    public MachineBlock(MachineLevel master, Block block, Material m, boolean explodeEffect, boolean smokeEffect, boolean sparkleEffect) {
+        this.setMasterLevel(master);
+        this.setBlock(block);
+        this.setExplodeEffect(explodeEffect);
+        this.setSmokeEffect(smokeEffect);
+        this.setSparkleEffect(sparkleEffect);
     }
 
 }
