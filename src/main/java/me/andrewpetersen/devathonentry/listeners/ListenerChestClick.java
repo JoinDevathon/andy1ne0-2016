1f28c114380688ebd8adc47cf349dd8ff9dac274
Andrew <andy1ne0@gmail.com>
2016-11-06 15:28:36 +1000

    Some ground work on creating the chest listener. Need to create a ItemBuilder class real quick.

--- src/main/java/me/andrewpetersen/devathonentry/listeners/ListenerChestClick.java
+++ src/main/java/me/andrewpetersen/devathonentry/listeners/ListenerChestClick.java
@@ -1,10 +1,17 @@
 package me.andrewpetersen.devathonentry.listeners;
 
+import me.andrewpetersen.devathonentry.Strings;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
 
 /*
  * This project has been written by Andrew Petersen, and anyone who has contributed to the source code
@@ -24,7 +31,18 @@ public class ListenerChestClick implements Listener {
         if (evt.getClickedBlock() == null) return;
         if (evt.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (evt.getHand() != EquipmentSlot.HAND) return;
-
+        if (evt.getClickedBlock().getType() == Material.ENDER_CHEST) {
+            Location loc = evt.getClickedBlock().getLocation();
+            loc.setY(loc.getY() - 2);
+            if (loc.getBlock() != null && loc.getBlock().getState() instanceof Sign) {
+                Sign s = (Sign) loc.getBlock().getState();
+                if (s.getLine(0).equals(Strings.SIGN_MACHINE_PREFIX_INTERNAL)) {
+                    evt.setCancelled(true);
+                    Inventory inv = Bukkit.createInventory(null, InventoryType.FURNACE, Strings.ENDER_CHEST_TITLE);
+                    
+                }
+            }
+        }
 
     }
 
